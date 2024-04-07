bd4dae6b8a4a03fa3c5200901fbae7e2e48e5343
GoldSonBR <gugadantas2010@gmail.com>
2024-04-07 20:01:01 -0300

    feat: Implementing get event route

--- src/data/event_handler.py
+++ src/data/event_handler.py
@@ -3,6 +3,7 @@ from src.models.repository.events_repository import EventsRepository
 from src.http_types.http_request import HttpRequest
 from src.http_types.http_response import HttpResponse
 
+
 class EventHandler:
     def __init__(self) -> None:
         self.__events_repository = EventsRepository()
@@ -14,5 +15,26 @@ class EventHandler:
 
         return HttpResponse(
             body={ "eventId": body["uuid"] },
+            status_code=200
+        )
+    
+    def find_by_id(self, http_request: HttpRequest) -> HttpResponse:
+        event_id = http_request.param["event_id"]
+        event = self.__events_repository.get_event_by_id(event_id)
+        if not event: raise ("Evento não encontrado")
+        
+        event_attendees_count = self.__events_repository.count_event_attendees(event_id)
+
+        return HttpResponse(
+            body={
+                "event": {
+                    "id": event.id,
+                    "title": event.title,
+                    "detail": event.details,
+                    "slug": event.slug,
+                    "maximumAttendees": event.maximum_attendees,
+                    "attendeesAmount": event_attendees_count["attendeesAmount"]
+                }
+            },
             status_code=200
         )
